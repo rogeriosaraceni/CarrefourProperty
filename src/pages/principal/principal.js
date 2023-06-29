0e27a6745bdfffe330dcfc9774dd59bd3abc55cd
rogeriosaraceni <rsaraceni@gmail.com>
2023-06-29 14:30:29 -0300

    ajuste de cores Principal

--- src/pages/principal/principal.js
+++ src/pages/principal/principal.js
@@ -1,37 +1,37 @@
 const cards = [
     {
         link: "../ficha-tecnica/index.html",
-        icon: "bi bi-file-earmark-ruled",
+        icon: "bi bi-file-earmark-ruled text-app-green-400",
         name: "Ficha Técnica",
         nameClassColor: "bg-app-green-400",
     },
     {
         link: "../dos/index.html",
-        icon: "bi bi-bar-chart",
+        icon: "bi bi-bar-chart text-app-cyan-400",
         name: "DOS",
         nameClassColor: "bg-app-cyan-400",
     },
     {
         link: "../manutencao/index.html",
-        icon: "bi bi-gear",
+        icon: "bi bi-gear text-app-blue-400",
         name: "Manutenção",
         nameClassColor: "bg-app-blue-400",
     },
     {
         link: "../operacoes-galerias/index.html",
-        icon: "bi bi-columns",
+        icon: "bi bi-columns text-app-pink-700",
         name: "Operações Galerias",
         nameClassColor: "bg-app-pink-700",
     },
     {
         link: "../comercial/index.html",
-        icon: "bi bi-buildings",
+        icon: "bi bi-buildings text-app-red-400",
         name: "Comercial",
         nameClassColor: "bg-app-red-400",
     },
     {
         link: "../projetos-galerias/page-planta-galeria.html",
-        icon: "bi bi-clipboard2-check",
+        icon: "bi bi-clipboard2-check text-app-orange-400",
         name: "Projetos Galerias",
         nameClassColor: "bg-app-orange-400",
     },
