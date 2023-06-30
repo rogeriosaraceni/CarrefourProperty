022652d573bba8fc91a6dcf375d93bc4c1675bb9
rogeriosaraceni <rsaraceni@gmail.com>
2023-06-30 06:57:10 -0300

    corrigindo const sanitize

--- src/assets/core/js/core-main.js
+++ src/assets/core/js/core-main.js
@@ -18,12 +18,12 @@
 /*! --------------------------------------------------------------------
  * DOMPurif protect xss
  * --------------------------------------------------------------------*/
-const sanitize = (string) => DOMPurify.sanitize(string);
+const sanitizeXss = (string) => DOMPurify.sanitize(string);
 
 let inputsApp = document.querySelectorAll(".form-control");
 inputsApp.forEach((item) => {
     item.addEventListener("change", (e) => {
-        const result = sanitize(e.target.value);
+        const result = sanitizeXss(e.target.value);
         console.log(result);
     });
 });
