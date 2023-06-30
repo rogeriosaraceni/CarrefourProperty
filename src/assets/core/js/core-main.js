735f3324ca21deeb2e9914a1166deea872871de0
rogeriosaraceni <rsaraceni@gmail.com>
2023-06-30 07:29:21 -0300

    corrigindo js

--- src/assets/core/js/core-main.js
+++ src/assets/core/js/core-main.js
@@ -14,13 +14,7 @@
  *
  * //////////////////////////////////////////////////////////////////// */
 
-/*! --------------------------------------------------------------------
- * Enable tooltips
- * --------------------------------------------------------------------*/
-addEventListener("DOMContentLoaded", function () {
-    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
-    const tooltipList = [...tooltipTriggerList].map((tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl));
-})
+
 
 /*! --------------------------------------------------------------------
 * mostra senha
